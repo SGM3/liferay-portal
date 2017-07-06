5abf14a32a065cd56aa505670efb9baa35e47c6c
Shanon Mathai <shanon.mathai@liferay.com>
2017-07-06 08:57:12 -0500

    InetSocketTransportAddress replaced https://github.com/elastic/elasticsearch/pull/24205

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
@@ -40,7 +40,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.logging.LogConfigurator;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
@@ -110,7 +110,7 @@ public class RemoteElasticsearchConnection extends BaseElasticsearchConnection {
 		InetAddress inetAddress = InetAddress.getByName(host);
 
 		transportClient.addTransportAddress(
-			new InetSocketTransportAddress(inetAddress, port));
+			new TransportAddress(inetAddress, port));
 	}
 
 	@Override
